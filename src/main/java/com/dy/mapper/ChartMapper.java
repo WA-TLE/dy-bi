2a4ea87a83f26480371adfa24252f37dcc2b537c
dy <dyu32345@gmail.com>
2024-04-16 21:50:05 +0800

    增加 User, Chart 实体类, 删除微信登录相关代码

---
+++ src/main/java/com/dy/mapper/ChartMapper.java
@@ -0,0 +1,18 @@
+package com.dy.mapper;
+
+import com.dy.model.entity.Chart;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 微光
+* @description 针对表【chart(图表信息表)】的数据库操作Mapper
+* @createDate 2024-04-16 20:26:47
+* @Entity com.dy.model.entity.chart
+*/
+public interface ChartMapper extends BaseMapper<Chart> {
+
+}
+
+
+
+
---
+++ src/main/java/com/dy/service/ChartService.java
@@ -0,0 +1,13 @@
+package com.dy.service;
+
+import com.dy.model.entity.Chart;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 微光
+* @description 针对表【chart(图表信息表)】的数据库操作Service
+* @createDate 2024-04-16 20:26:47
+*/
+public interface ChartService extends IService<Chart> {
+
+}
